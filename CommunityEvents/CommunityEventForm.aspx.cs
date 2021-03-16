a4b953264a3cb96d3090147d9d35009c93c1ea8e
shoemaker30 <72107198+shoemaker30@users.noreply.github.com>
2021-03-16 18:22:14 -0400

    added button click to get events test

--- CommunityEvents/CommunityEventForm.aspx.cs
+++ CommunityEvents/CommunityEventForm.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using CommunityEvents.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace CommunityEvents
 {
@@ -16,7 +19,33 @@ namespace CommunityEvents
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            TextBox1.Text = "test";
+            IEnumerable<Event> dataObjects = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44324/api/event");
+
+                // Add an Accept header for JSON format.
+                client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //HTTP GET
+                HttpResponseMessage response = client.GetAsync("").Result;
+              
+
+                if (response.IsSuccessStatusCode)
+                {
+                    dataObjects = response.Content.ReadAsAsync<IEnumerable<Event>>().Result;
+                    
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                }
+            }
+           
+            TextBox1.Text = dataObjects.ToString();
         }
     }
 }
