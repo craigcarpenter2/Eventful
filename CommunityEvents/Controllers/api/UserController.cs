092a8466fa3429368b5a8bc76f4c13581064ca34
Eric Shoemaker <shoemaker30@live.marshall.edu>
2021-03-23 16:33:16 -0400

    class 2-23-21 beginning

---
+++ CommunityEvents/Controllers/api/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CommunityEvents.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CommunityEvents.Controllers.api
+{
+    public class UserController : ApiController
+    {
+        List<User> users = new List<User>()
+        {
+            new User()
+            {
+                Username = "Eric",
+                Password = "Shoemaker"
+                
+            }
+            
+        };
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(String username, String password)
+        {
+            return 
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CommunityEvents/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CommunityEvents.Models
+{
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public String Username { get; set; }
+
+        [Required]
+        public String Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int HomeZip { get; set; }
+    }
+}
